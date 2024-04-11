5ab4e6547b9e0855490f9e960a7ed451600d41dd
LambertoMaia <lambertomaia1@gmail.com>
2024-04-11 15:39:53 -0300

    Initial commit

---
+++ src/main/java/com/restaurante/restauapi/RestauapiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.restauapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurante/restauapi/RestauapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.restauapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
